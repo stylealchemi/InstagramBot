4b14377a38eabcfe7147ea9a405ac6ee13729d11
StyleAlchemi <stylealchemi857@gmail.com>
2025-02-14 14:28:15 +0100

    Add files via upload

--- basebot.py
+++ basebot.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 import random
+import gunicorn
 
 # Initialize Flask app
 app = Flask(__name__)

8d783f7ed11e7e63ad64532ff7780a43cd298d6d
StyleAlchemi <stylealchemi857@gmail.com>
2025-02-23 18:05:52 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, jsonify
+import threading
+from automation import start_automation
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/start', methods=['POST'])
+def start():
+    data = request.get_json()
+    threading.Thread(target=start_automation, args=(data,)).start()
+    return jsonify({"status": "Automation started"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
